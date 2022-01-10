24bde1c42c6054f08ced814bd314426e9b363df5
unknown <ibrahim.mo.elsayed@gmail.com>
2022-01-10 17:17:25 +0200

    I changed the data of light bar and added the logic

--- library/main.c
+++ library/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Book.h"
-#include "dynamicArray.h"
+//#include "Book.h"
+//#include "ynamicArray.h"
 #include <windows.h>
 COORD coord= {0,0};
 void gotoxy(int x,int y);
@@ -17,10 +17,14 @@ int main()
     position=1;
     do {
             SetConsoleTextAttribute(console,15);
-            gotoxy(40,11);printf("[1] Type your name                 ");
-            gotoxy(40,12);printf("[2] Type your name                 ");
-            gotoxy(40,13);printf("[3] Type your name                 ");
-            gotoxy(40,14);printf("[4] exit                           ");
+            gotoxy(42,9 );printf("[1] Add Book                       ");
+            gotoxy(42,10);printf("[2] Remove Book                    ");
+            gotoxy(42,11);printf("[3] Display the data of books      ");
+            gotoxy(42,12);printf("[4] Save data                      ");
+            gotoxy(42,13);printf("[5] Load data                      ");
+            gotoxy(42,14);printf("[6] Add user                       ");
+            gotoxy(42,15);printf("[7] Login user                     ");
+            gotoxy(42,16);printf("[8] Exit                           ");
             printf("\n\n");
 
 
@@ -28,19 +32,35 @@ int main()
             {
             case 1:
                 SetConsoleTextAttribute(console,240);
-                gotoxy(40,11);printf("[1] Type your name                 ");
+                gotoxy(42,9);printf("[1] Add Book                       ");
                 break;
             case 2:
                 SetConsoleTextAttribute(console,240);
-                gotoxy(40,12);printf("[2] Type your name                 ");
+                gotoxy(42,10);printf("[2] Remove Book                    ");
                 break;
             case 3:
                 SetConsoleTextAttribute(console,240);
-                gotoxy(40,13);printf("[3] Type your name                 ");
+                gotoxy(42,11);printf("[3] Display the data of books      ");
                 break;
             case 4:
                 SetConsoleTextAttribute(console,240);
-                gotoxy(40,14);printf("[4] exit                           ");
+                gotoxy(42,12);printf("[4] Save data                      ");
+                break;
+            case 5:
+                SetConsoleTextAttribute(console,240);
+                gotoxy(42,13);printf("[5] Load data                      ");
+                break;
+            case 6:
+                SetConsoleTextAttribute(console,240);
+                gotoxy(42,14);printf("[6] Add user                       ");
+                break;
+            case 7:
+                SetConsoleTextAttribute(console,240);
+                gotoxy(42,15);printf("[7] Login user                     ");
+                break;
+            case 8:
+                SetConsoleTextAttribute(console,240);
+                gotoxy(42,16);printf("[8] Exit                           ");
                 break;
 
             }
@@ -49,12 +69,12 @@ int main()
             if(ch==72)
             {
                 position--;
-                if(position==0)position=4;
+                if(position==0)position=8;
             }
             if(ch==80)
             {
                 position++;
-                if(position==5)position=1;
+                if(position==9)position=1;
             }
 
 
@@ -62,7 +82,41 @@ int main()
 
 gotoxy(40,18);
 SetConsoleTextAttribute(console,15);
-printf("your choose is : %d\n",position);
+switch(position)
+{
+case 1:
+    //add fun of add book
+    //like ==> add();
+    break;
+case 2:
+    //add fun of remove book
+    //like ==> add();
+    break;
+case 3:
+    //add fun of display the data of books
+    //like ==> add();
+    break;
+case 4:
+   //add fun of save data
+    //like ==> add();
+    break;
+case 5:
+    //add fun of load data
+    //like ==> add();
+    break;
+case 6:
+    //add fun of add user
+    //like ==> add();
+    break;
+case 7:
+    //add fun of login user
+    //like ==> add();
+    break;
+case 8:
+    exit(0);
+
+
+}
 
 }
 
@@ -75,32 +129,44 @@ void gotoxy(int x,int y)
 void draw_box(void)
 {
     int i ,j;
-    gotoxy(39,10);printf("%c",201);
+    gotoxy(41,8);printf("%c",201);
     for (i=1;i<36;i++)printf("%c",205);
     printf("%c",187);
 
-    gotoxy(39,11);printf("%c",186);
+    gotoxy(41,9);printf("%c",186);
     for (i=1;i<36;i++)printf(" ");
     printf("%c",186);
 
 
-    gotoxy(39,12);printf("%c",186);
+    gotoxy(41,10);printf("%c",186);
     for (i=1;i<36;i++)printf(" ");
     printf("%c",186);
 
-    gotoxy(39,13);printf("%c",186);
+    gotoxy(41,11);printf("%c",186);
     for (i=1;i<36;i++)printf(" ");
     printf("%c",186);
 
-    gotoxy(39,14);printf("%c",186);
+    gotoxy(41,12);printf("%c",186);
     for (i=1;i<36;i++)printf(" ");
     printf("%c",186);
 
-     gotoxy(39,15);printf("%c",186);
+     gotoxy(41,13);printf("%c",186);
     for (i=1;i<36;i++)printf(" ");
     printf("%c",186);
 
-    gotoxy(39,16);printf("%c",200);
+    gotoxy(41,14);printf("%c",186);
+    for (i=1;i<36;i++)printf(" ");
+    printf("%c",186);
+
+    gotoxy(41,15);printf("%c",186);
+    for (i=1;i<36;i++)printf(" ");
+    printf("%c",186);
+
+    gotoxy(41,16);printf("%c",186);
+    for (i=1;i<36;i++)printf(" ");
+    printf("%c",186);
+
+    gotoxy(41,17);printf("%c",200);
     for (i=1;i<36;i++)printf("%c",205);
     printf("%c",188);
 
